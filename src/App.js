084e04135b14290494b6fd0ba6b29b9e9c1bbfa2
Sahalu Aminu <sahlminkok7@gmail.com>
2025-05-01 18:41:11 +0100

    Add previous and next buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import songs from './data/songs';
 
 function App() {
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const audioRef = useRef(null);
+
+  function playNext() {
+    setCurrentSongIndex((prevIndex) =>
+      prevIndex === songs.length - 1 ? 0 : prevIndex + 1
+    );
+  }
+
+  function playPrevious() {
+    setCurrentSongIndex((prevIndex) =>
+      prevIndex === 0 ? songs.length - 1 : prevIndex - 1
+    );
+  }
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.load();
+      audioRef.current.play();
+    }
+  }, [currentSongIndex]);
 
   return (
     <div className="App">
@@ -16,6 +35,15 @@ function App() {
         <track kind="captions" src="" label="English captions" />
         Your browser does not support the audio element.
       </audio>
+
+      <div>
+        <button type="button" onClick={playPrevious}>
+          Previous
+        </button>
+        <button type="button" onClick={playNext}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
