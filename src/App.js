68cdde7ce88be9bec33dead8b322816da8b2d031
Sahalu Aminu <sahlminkok7@gmail.com>
2025-05-01 18:38:31 +0100

    Import songs and render it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useRef, useState } from 'react';
+import songs from './data/songs';
+
 function App() {
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const audioRef = useRef(null);
+
   return (
     <div className="App">
-      <h1>Hello, World</h1>
+      <h2>
+        {songs[currentSongIndex].title} - {songs[currentSongIndex].artist}
+      </h2>
+
+      <audio ref={audioRef} controls autoPlay>
+        <source src={songs[currentSongIndex].src} type="audio/mpeg" />
+        <track kind="captions" src="" label="English captions" />
+        Your browser does not support the audio element.
+      </audio>
     </div>
   );
 }
