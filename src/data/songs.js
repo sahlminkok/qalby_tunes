691c7ed91d291c10c12f21a3db7385095748561b
Sahalu Aminu <sahlminkok7@gmail.com>
2025-05-01 18:36:32 +0100

    Create an array of songs

---
+++ src/data/songs.js
@@ -0,0 +1,28 @@
+const songs = [
+  {
+    id: 1,
+    title: 'Ya Adheeman',
+    artist: 'Ahmed Bukhater',
+    src: '/audio/AhmedBukhater-Yaadheeman.mp3',
+  },
+  {
+    id: 2,
+    title: 'Qamarun',
+    artist: 'Mostafa Atef',
+    src: '/audio/Mostafa_Atef_-_Qamarun_-_(Hydr0.org).mp3',
+  },
+  {
+    id: 3,
+    title: 'Ya Quluban',
+    artist: '',
+    src: '/audio/Nasheed_-_Ya_Quluban_(Hydr0.org).mp3',
+  },
+  {
+    id: 4,
+    title: 'The Beauty of Existence',
+    artist: 'Muhammad Al_muqeet',
+    src: '/audio/The_Beauty_of_Existence_-_Heart_Touching_Nasheed.mp3',
+  },
+];
+
+export default songs;
